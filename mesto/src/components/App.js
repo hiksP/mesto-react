5f631f6919aa195b6d46bcb6e0a55eec4110b754
Peter Plavskikh <qksyo@yandex.ru>
2022-03-04 00:07:15 +0700

    zagatovki

--- mesto/src/components/App.js
+++ mesto/src/components/App.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../index.css';
 import {Header} from './Header.js';
 import {Main} from './Main.js';
 import {Footer} from './Footer.js';
 
 function App() {
+
+const handleEditAvatarClick = (evt) => {
+  console.log(evt)
+}
+
+const handleEditProfileClick = (evt) => {
+  console.log(evt)
+}
+
+const handleAddPlaceClick = (evt) => {
+  console.log(evt)
+}
+
   return (
   <>
     <div className="page">
       <div className="page__size">
         <Header/>
-      <Main />
+      <Main
+      editAvatarPopup={handleEditAvatarClick}
+      editProfilePopup={handleEditProfileClick}
+      addCardPopup={handleAddPlaceClick}
+        />
       <Footer />
     </div>
     <div className="popup popup_edit">
       <div className="popup__overlay"></div>
       <div className="popup__container">
         <button className="popup__close popup__close_edit" type="button"></button>
-        <form className="popup__form popup__form_edit" novalidate name="data-edit">
+        <form className="popup__form popup__form_edit" noValidate name="data-edit">
           <h3 className="popup__title">Редактировать профиль</h3>
           <ul className="popup__list">
             <li className="popup__list-element">
-              <input type="text" className="popup__data-box" required minlength="2" maxlength="40" name="name" placeholder="Имя пользователя"/>
+              <input type="text" className="popup__data-box" required minLength="2" maxLength="40" name="name" placeholder="Имя пользователя"/>
               <span className="popup__input-error" id="name-error"></span>
             </li>
             <li className="popup__list-element">
-              <input type="text" className="popup__data-box" required minlength="2" maxlength="200" name="status" placeholder="Профессиональная деятельность"/>
+              <input type="text" className="popup__data-box" required minLength="2" maxLength="200" name="status" placeholder="Профессиональная деятельность"/>
               <span className="popup__input-error" id="status-error"></span>
             </li>
           </ul>
@@ -37,15 +54,15 @@ function App() {
       <div className="popup__overlay"></div>
       <div className="popup__container">
         <button className="popup__close popup__close_add" type="button"></button>
-        <form className="popup__form popup__form_add" novalidate name="data-add">
+        <form className="popup__form popup__form_add" noValidate name="data-add">
           <h3 className="popup__title">Новое место</h3>
           <ul className="popup__list">
             <li className="popup__list-element">
-              <input type="text" className="popup__data-box" name="place-name" required minlength="2" maxlength="30" placeholder="Название"/>
+              <input type="text" className="popup__data-box" name="place-name" required minLength="2" maxLength="30" placeholder="Название"/>
               <span className="popup__input-error" id="place-name-error"></span>
             </li>
             <li className="popup__list-element">
-              <input type="url" class="popup__data-box" name="place-link" required placeholder="Сылка на картинку"/>
+              <input type="url" className="popup__data-box" name="place-link" required placeholder="Сылка на картинку"/>
               <span className="popup__input-error" id="place-link-error"></span>
             </li>
           </ul>
@@ -75,7 +92,7 @@ function App() {
       <div className="popup__overlay"></div>
       <div className="popup__container">
         <button className="popup__close popup__close_change" type="button"></button>
-        <form className="popup__form popup__form_change" novalidate name="data-change">
+        <form className="popup__form popup__form_change" noValidate name="data-change">
           <h3 className="popup__title">Обновить аватар</h3>
           <ul className="popup__list">
             <li className="popup__list-element">
--- mesto/src/components/Main.js
+++ mesto/src/components/Main.js
@@ -1,14 +1,14 @@
-export function Main() {
+export function Main({editAvatarPopup, editProfilePopup, addCardPopup}) {
     return(
         <main>
         <section className="profile">
-          <a className="profile__avatar-hover" href="#"><img className="profile__avatar" alt="Аватар профиля"/></a>
+          <a className="profile__avatar-hover" href="#"><img className="profile__avatar" onClick={editAvatarPopup} alt="Аватар профиля"/></a>
           <div className="profile__info">
             <h1 className="profile__name"></h1>
-            <button className="profile__edit-button" type="button"></button>
+            <button className="profile__edit-button" type="button" onClick={editProfilePopup}></button>
             <p className="profile__status"></p>
           </div>
-          <button className="profile__add-button" type="button"></button>
+          <button className="profile__add-button" type="button" onClick={addCardPopup}></button>
         </section>
         <section className="elements">
           <ul className="elements__list">
